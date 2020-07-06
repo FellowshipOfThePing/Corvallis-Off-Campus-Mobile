07c806e2a021e2cf55c9d029a4bf6cc35c1b7927
Zachary Tindell <tempe85@gmail.com>
2020-07-06 16:54:56 -0700

    Reformatted icons

--- app/components/IconRow.js
+++ app/components/IconRow.js
@@ -1,71 +1,40 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import {
-  MaterialCommunityIcons,
-  FontAwesome,
-  FontAwesome5,
-} from "@expo/vector-icons";
-import colors from "../config/colors";
-import AppText from "../components/AppText";
+import IconWithText from "./IconWithText";
 
 function IconRow({ listing, style }) {
   return (
     <View style={[styles.iconRow, style]}>
-      {listing.beds && (
-        <>
-          <FontAwesome
-            name="bed"
-            size={20}
-            color={colors.medium}
-            style={styles.icon}
-          />
-          <AppText style={styles.iconText}>{listing.beds} bd</AppText>
-        </>
-      )}
-      {listing.baths && (
-        <>
-          <FontAwesome5
-            name="bath"
-            size={20}
-            color={colors.medium}
-            style={styles.icon}
-          />
-          <AppText style={styles.iconText}>{listing.baths} ba</AppText>
-        </>
-      )}
-      {listing.sqft && (
-        <>
-          <FontAwesome5
-            name="ruler-combined"
-            size={20}
-            color={colors.medium}
-            style={styles.icon}
-          />
-          <AppText style={styles.iconText}>{listing.sqft} sqft</AppText>
-        </>
-      )}
-      {listing.walk_to_campus_minutes < 30 && (
-        <>
-          <FontAwesome5
-            name="walking"
-            size={20}
-            color={colors.medium}
-            style={styles.icon}
-          />
-          <AppText style={styles.iconText}>{Math.round(listing.walk_to_campus_minutes)} min</AppText>
-        </>
-      )}
-      {listing.walk_to_campus_minutes > 30 && (
-        <>
-          <FontAwesome5
-            name="car-side"
-            size={20}
-            color={colors.medium}
-            style={styles.icon}
-          />
-          <AppText style={styles.iconText}>{Math.round(listing.drive_to_campus_minutes)} min</AppText>
-        </>
-      )}
+      <IconWithText
+        iconName="bed"
+        quantity={listing.beds}
+        textValue={"bd"}
+        visible={!!listing.beds}
+      />
+      <IconWithText
+        iconName="bath"
+        quantity={listing.baths}
+        textValue={"ba"}
+        visible={!!listing.baths}
+      />
+      <IconWithText
+        iconName="ruler-combined"
+        quantity={listing.sqft}
+        textValue={"sqft"}
+        visible={!!listing.sqft}
+      />
+      <IconWithText
+        iconName="walking"
+        quantity={Math.round(listing.walk_to_campus_minutes)}
+        textValue={"min"}
+        visible={listing.walk_to_campus_minutes < 30}
+      />
+      <IconWithText
+        iconName="car-side"
+        quantity={Math.round(listing.drive_to_campus_minutes)}
+        textValue={"min"}
+        visible={listing.walk_to_campus_minutes > 30}
+      />
     </View>
   );
 }
@@ -73,6 +42,8 @@ function IconRow({ listing, style }) {
 const styles = StyleSheet.create({
   iconRow: {
     flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "red",
   },
   iconText: {
     marginHorizontal: 9,
---
+++ app/components/IconWithText.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { StyleSheet, useWindowDimensions } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+import AppText from "./AppText";
+import colors from "../config/colors";
+
+function IconWithText({
+  iconName,
+  iconSize = 20,
+  color = colors.medium,
+  quantity,
+  textValue,
+  visible,
+}) {
+  if (!visible) {
+    return null;
+  }
+
+  const windowWidth = useWindowDimensions().width;
+  let iconSizeOverride;
+  if (windowWidth < 500) {
+    iconSizeOverride = Math.round((iconSize * windowWidth) / 500);
+    console.log(iconSizeOverride);
+  }
+
+  return (
+    <>
+      <FontAwesome5
+        name={iconName}
+        size={iconSize || iconSizeOverride}
+        color={color}
+        style={styles.icon}
+      />
+      <AppText style={styles.iconText}>
+        {quantity} {textValue}
+      </AppText>
+    </>
+  );
+}
+
+export default IconWithText;
+
+const styles = StyleSheet.create({
+  iconText: {
+    marginHorizontal: 9,
+    fontSize: 15,
+  },
+  icon: {},
+});
