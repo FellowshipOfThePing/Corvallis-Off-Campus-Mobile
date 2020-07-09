dce7f45e73b8fb5070fc64aa3cbdcc4dc2b35b14
FellowshipOfThePing <jeremybinderdrummer@gmail.com>
2020-07-09 01:43:47 -0700

    Material Tabs added

---
+++ app/navigation/MaterialTabs.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+import FeedNavigator from "./FeedNavigator";
+import ExplorerScreen from "../screens/ExplorerScreen";
+import colors from "../config/colors";
+
+const Tab = createMaterialBottomTabNavigator();
+
+const MaterialTabs = () => {
+
+  return (
+    <Tab.Navigator shifting barStyle={{ backgroundColor: colors.primary }}>
+      <Tab.Screen
+        name="Feed"
+        component={FeedNavigator}
+        options={{
+          tabBarIcon: ({ focused, color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={25}/>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Map"
+        component={ExplorerScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="google-maps" color={color} size={25} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default MaterialTabs;
--- app/navigation/WelcomeNavigator.js
+++ app/navigation/WelcomeNavigator.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import AppNavigator from "./AppNavigator";
+import MaterialTabs from "./MaterialTabs";
 import WelcomeScreen from "../screens/WelcomeScreen";
 
 const Stack = createStackNavigator();
 
 const WelcomeNavigator = () => (
   <Stack.Navigator headerMode={"none"}>
-    <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    <Stack.Screen name="Welcome" component={MaterialTabs} />
     <Stack.Screen name="AppNavigator" component={AppNavigator} />
   </Stack.Navigator>
 );
