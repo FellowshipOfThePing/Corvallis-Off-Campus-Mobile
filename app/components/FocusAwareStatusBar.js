fa815609197086d0e6beccc28b65107394b01676
FellowshipOfThePing <jeremybinderdrummer@gmail.com>
2020-09-08 19:58:49 -0700

    Reverted firebase/netinfo/lottie packages back to original versions for podfile compatability. Minor layout tweaks. Build stable.

--- app/components/FocusAwareStatusBar.js
+++ app/components/FocusAwareStatusBar.js
@@ -2,11 +2,16 @@ import * as React from "react";
 import { StatusBar } from "react-native";
 import { useIsFocused } from "@react-navigation/native";
 
-function FocusAwareStatusBar({ barStyle, backgroundColor }) {
+function FocusAwareStatusBar({ barStyle, backgroundColor, hidden = false }) {
   const isFocused = useIsFocused();
 
   return isFocused ? (
-    <StatusBar barStyle={barStyle} backgroundColor={backgroundColor} />
+    <StatusBar
+      barStyle={barStyle}
+      backgroundColor={backgroundColor}
+      animated
+      hidden={hidden}
+    />
   ) : null;
 }
 
